6c5df16676d9db36a8f700c9670523ef237f8255
Darren Chaddock <dchaddoc@ucalgary.ca>
2024-06-17 23:01:43 +0000

    version bump to 1.0.0

--- pyucalgarysrs/__init__.py
+++ pyucalgarysrs/__init__.py
@@ -33,7 +33,7 @@ Basic usage:
 """
 
 # versioning info
-__version__ = "0.0.36"
+__version__ = "1.0.0"
 
 # documentation
 __pdoc__ = {"pyucalgarysrs": False}
--- tests/test_suite/test_version.py
+++ tests/test_suite/test_version.py
@@ -16,4 +16,4 @@ from pyucalgarysrs import __version__
 
 
 def test_version():
-    assert __version__ == "0.0.36"
+    assert __version__ == "1.0.0"
